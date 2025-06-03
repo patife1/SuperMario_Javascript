e8c10497e8c98f97c370be012128b22901be8f92
Izabelly Márcia <santosizabelly6@gmail.com>
2025-06-03 16:24:07 -0300

    PULO

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-
 const mario = document.querySelector('.mario');
 
-const jump = () => {
-    mario.classList.add('jump');
-
-    setTimeout(() => {
-
-         mario.classList.remove('jump');
-         
-    }, 500);
-}
-
-
-
-document.addEventListener('keydown', jump);
+document.addEventListener('keydown', (e) => {
+    if (
+        e.code === 'Space' ||
+        e.key === ' ' ||
+        e.key === 'ArrowUp'
+    ) {
+        if (!mario.classList.contains('jump')) {
+            mario.classList.add('jump');
+            setTimeout(() => {
+                mario.classList.remove('jump');
+            }, 500);
+        }
+    }
+});
